63cc0fa32b3c5321e048ac84f7a0bcceeef8354e
HanqinDu <43931278+HanqinDu@users.noreply.github.com>
2024-10-04 16:18:15 +0100

    Update 2_train_miRNA.sh
    change example script to XGB-miRNA with default hyperparameter

--- 2_train_miRNA.sh
+++ 2_train_miRNA.sh
@@ -2,10 +2,10 @@ conda activate TMZ-LGG
 
 python src/evaluate_model.py \
 -i data/dataset/omics_1110/dm_miRNA.feather \
--o data/prediction/XGB_miRNA.pickle \
--c XGB-C \
--j 5 \
+-o data/prediction/XGB_default_miRNA.pickle \
+-c XGB-C-D \
+-s 128 46 59124 1889 6 \
 -f 5 \
--p 2 \
--r 50 \
--s 128 46 59124 1889 6
+-j 5 \
+-a
+
